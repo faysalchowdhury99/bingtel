87c822891c8cdfa1ba2f3934dc5cd36530cd0ae3
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-02-23 12:25:13 +0600

    Some Features Changed

--- src/js/index.js
+++ src/js/index.js
@@ -11,25 +11,6 @@ navbarToggler.addEventListener('click', () => {
     }
 });
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-    stickyFunc();
-};
-
-// Get the navbar
-const navbar = document.querySelector('.navbar');
-
-// Get the offset position of the navbar
-const sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyFunc() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add('sticky-nav');
-    } else {
-        navbar.classList.remove('sticky-nav');
-    }
-}
 // Download Slider
 const swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
@@ -37,6 +18,9 @@ const swiper = new Swiper('.swiper-container', {
     grabCursor: false,
     centeredSlides: true,
     slidesPerView: 'auto',
+    autoplay: {
+        delay: 5000,
+    },
     coverflowEffect: {
         rotate: 0,
         stretch: 300,
